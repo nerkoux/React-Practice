598097ecf44ce4c085038a1827a90d61aa2e99ba
Akshat Mehta <2023pietcaakshat004@poornima.org>
2024-07-19 10:02:11 +0530

    added hamburger menu also made it responsive for all the devices

--- assignment1/src/component/Navbar.jsx
+++ assignment1/src/component/Navbar.jsx
@@ -1,25 +1,39 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 const Navbar = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
           Akshat Mehta
         </Link>
-        <ul className="nav-menu">
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={`nav-menu ${isMenuActive ? 'active' : ''}`}>
           <li className="nav-item">
-            <Link to="/" className="nav-link">Functional Components</Link>
+            <Link to="/" className="nav-link" onClick={toggleMenu}>Functional Components</Link>
           </li>
           <li className="nav-item">
-            <Link to="/routingcheck" className="nav-link">Routing Check</Link>
-            <Link to="/userlist" className="nav-link">User List</Link>
-
+            <Link to="/routingcheck" className="nav-link" onClick={toggleMenu}>Routing Check</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/userlist" className="nav-link" onClick={toggleMenu}>User List</Link>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
